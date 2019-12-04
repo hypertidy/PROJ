958d82290b1014275fa16765ce12e2e7f8325aca
Michael Sumner <mdsumner@gmail.com>
2019-12-04 11:40:28 +0000

    fix tests

--- tests/testthat/test-PROJ.R
+++ tests/testthat/test-PROJ.R
@@ -6,8 +6,11 @@ lat <- na.omit(w[,2])
 dst <- "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42"
 
 test_that("multiplication works", {
-  expect_silent(xyz <- proj_trans(dst, X = lon, Y = lat, Z = rep(0, length(lon)), INV = FALSE))
-  expect_silent(lonlat <- proj_trans(dst, X = xyz[, 1L], Y = xyz[, 2L], Z = rep(0, length(lon)), INV = TRUE))
+  if (!ok_proj6()) skip()
+
+  expect_silent(xyz <- proj_trans_generic(cbind(X = lon, Y = lat, Z = rep(0, length(lon))), dst, source = "WGS84"))
+  expect_silent(lonlat <- proj_trans_generic(cbind(X = xyz[, 1L], Y = xyz[, 2L], Z = rep(0, length(lon))),
+                                     "WGS84", source = dst))
   expect_true(all(lonlat[, 1L] <= 180))
   expect_true(all(lonlat[, 1L] > -180))
   expect_true(all(lonlat[, 2L] < 90))
