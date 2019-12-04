d8bbf63c50a9d43b9cc35709cec6caa165085e8e
Michael Sumner <mdsumner@gmail.com>
2019-12-04 05:42:18 +0000

    should workd

--- R/proj_trans_generic.R
+++ R/proj_trans_generic.R
@@ -1,9 +1,9 @@
-proj_trans_generic <- function(source, target, n,
-                                   x, y, z, t, success) {
-  .C(C_proj_trans_generic,
-          as.character(source), as.character(target),
-          as.integer(n),
-          x_ = as.double(x), y_ = as.double(y), z_ = as.double(z), t_ = as.double(t),
-          success = as.integer(success),
-          NAOK=TRUE, PACKAGE = "PROJ")
-}
+# proj_trans_generic <- function(source, target, n,
+#                                    x, y, z, t, success) {
+#   .C(C_proj_trans_generic,
+#           as.character(source), as.character(target),
+#           as.integer(n),
+#           x_ = as.double(x), y_ = as.double(y), z_ = as.double(z), t_ = as.double(t),
+#           success = as.integer(success),
+#           NAOK=TRUE, PACKAGE = "PROJ")
+# }
