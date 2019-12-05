fbb0658b970c0918ae116a5799fe9f5c00c3b878
Michael Sumner <mdsumner@gmail.com>
2019-12-05 21:56:13 +0000

    PROJ.4

---
+++ ci/travis/proj4/before_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# set -e
+#
+# export chroot="$PWD"/bionic
+# mkdir -p "$chroot$PWD"
+
+export hr="$PWD"
+
+
+sudo apt-get update
+sudo apt-get upgrade
+
+wget https://download.osgeo.org/proj/proj-4.9.3.tar.gz
+(mv proj* /tmp; cd /tmp; tar xzf proj-4.9.3.tar.gz)
+(cd /tmp/proj-5.2.0; ./configure; make; sudo make install; sudo ldconfig)
+
+cd $hr
+
