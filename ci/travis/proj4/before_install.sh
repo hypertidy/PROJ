3dc526250f243f7f7208cedf98dae505d038ad3c
Michael Sumner <mdsumner@gmail.com>
2019-12-05 22:04:36 +0000

    doh

--- ci/travis/proj4/before_install.sh
+++ ci/travis/proj4/before_install.sh
@@ -13,7 +13,7 @@ sudo apt-get upgrade
 
 wget https://download.osgeo.org/proj/proj-4.9.3.tar.gz
 (mv proj* /tmp; cd /tmp; tar xzf proj-4.9.3.tar.gz)
-(cd /tmp/proj-5.2.0; ./configure; make; sudo make install; sudo ldconfig)
+(cd /tmp/proj-4.9.3; ./configure; make; sudo make install; sudo ldconfig)
 
 cd $hr
 
