c8173f7205f83fe05bfe022fcdeb1f64799667bd
Michael Sumner <mdsumner@gmail.com>
2019-12-11 23:26:29 +0000

    doc

--- R/PROJ-package.R
+++ R/PROJ-package.R
@@ -6,9 +6,23 @@
 ## usethis namespace: start
 #' @useDynLib PROJ, PROJ_proj_trans_generic
 #' @useDynLib PROJ, PROJ_set_data_dir
-
 ## usethis namespace: end
 NULL
+
+#' Generic Coordinate System Transformations Using 'PROJ'
+#'
+#' The goal of PROJ is to provide generic coordinate system transformations in R
+#' without requiring bespoke formats for storing basic data.
+#' @section I. Functions:
+#' \tabular{ll}{
+#'   \code{\link{ok_proj6}} determine if PROJ version >=6 is available
+#'    \code{\link{proj_trans_generic}} a light wrapper around the underlying transformation functionality of PROJ version 6 (or higher)
+#'  }
+#' @name PROJ-package
+#' @docType package
+NULL
+
+
 
 #' xymap data for testing
 #'
