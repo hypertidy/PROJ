07281ad4419d07450eaef0364fd68c8cbc70fb9d
Michael Sumner <mdsumner@gmail.com>
2019-12-18 05:14:32 +0000

    list-io

--- R/proj_trans_generic.R
+++ R/proj_trans_generic.R
@@ -1,52 +1,50 @@
 #' Transform a set of coordinates with 'PROJ'
 #'
 #' A raw interface to proj_trans_generic in 'PROJ => 6', if it is available.
+#'
+#' Input 'x' is assumed to be 2-columns of "x", then "y" coordinates. If "z" or
+#' "t" is required pass these in as named vectors with "z_" and "t_". These
+#' are left empty (zero-length) internally by default.
 #' @param source projection of input coordinates (must be named)
 #' @param target projection for output coordinates
-#' @param x input coordinates
+#' @param x input coordinates (x,y, list or matrix see `z_` and `t_`)
 #' @param ... ignored
+#' @param z_ optional z coordinate vector
+#' @param t_ optional t coordinate vector
 #' @export
-#' @return matrix of transformed coordinates, with 4-columns x, y, z, t
+#' @return list of transformed coordinates, with 4-elements `x_`, `y_`, `z_`, `t_`
 #' @references see the [PROJ library documentation](https://proj.org/development/reference/functions.html#coordinate-transformation)
 #' for details on the underlying functionality
 #' @examples
 #' if (ok_proj6()) {
 #'  proj_trans_generic(cbind(147, -42), "+proj=laea", source = "epsg:4326")
+#'  proj_trans_generic(cbind(147, -42), z_ = -2, "+proj=laea", source = "epsg:4326")
+#'  proj_trans_generic(cbind(147, -42), z_ = -2, t_ = 1, "+proj=laea", source = "epsg:4326")
 #'  }
-proj_trans_generic <- function(x, target, ..., source = NULL) {
+proj_trans_generic <- function(x, target, ..., source = NULL, z_ = 0, t_ = numeric(0)) {
   ## don't run this if the basic test fails
   #tst <- getOption("PROJ.HAVE_PROJ6")
   #if (!tst) stop("no PROJ6 available (you could use proj4 package, via reproj)")
 
   if (missing(target) | !is.character(target)) stop("target must be a string")
   if (is.null(source) | !is.character(source)) stop("source must be provided as a string")
-
+  if (is.list(x) && !is.data.frame(x)) x <- as.data.frame(x[1:2])
+  if (dim(x)[2L] != 2L) stop("x coordinates must be 2-columns")
   if (is.data.frame(x)) x <- as.matrix(x)
   n <- dim(x)[1L]
   if (!is.numeric(x)) stop("input coordinates must be numeric")
   if (n < 1) stop("must be at least one coordinate")
-  if (dim(x)[2L] > 4) x <- x[,1:4, drop = FALSE]
-  if (dim(x)[2L] == 1) stop("coordinates must be 2-columns, 3-columns or 4-columns")
-  if (dim(x)[2L] == 2) x <- cbind(x, 0, 0)
-  if (dim(x)[2L] == 3) x <- cbind(x, 0)
-
-
-  ## reproj does all this stuff, and should also do the stuff above for this interface
-  # xx <- x
-  # ## now unpack those
-  t <- x[,4L, drop = TRUE]
-  z <- x[,3L, drop = TRUE]
   y <- x[,2L, drop = TRUE]
   x <- x[,1L, drop = TRUE]
   result <- .C("PROJ_proj_trans_generic",
            src_ = as.character(source), tgt_ = as.character(target),
            n = as.integer(n),
-           x_ = as.double(x), y_ = as.double(y), z_ = as.double(z), t_ = as.double(t),
+           x_ = as.double(x), y_ = as.double(y),
+           z_ = as.double(z_), t_ = as.double(t_),
            success = as.integer(0),
            NAOK=TRUE, PACKAGE = "PROJ")
   if (!result[["success"]]) stop("problem in PROJ transformation:\n(likely you don't have system PROJ version 6 or higher), WIP: see help in reproj package")
-  cbind(x = result[["x_"]], y = result[["y_"]],
-        z = result[["z_"]], t = result[["t_"]])
+  result[c("x_", "y_", "z_", "t_")]
 }
 
 
--- tests/testthat/test-sanity-inputs.R
+++ tests/testthat/test-sanity-inputs.R
@@ -6,28 +6,28 @@ dst <- "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42"
 llproj <- "epsg:4326"
 
 xy <- cbind(lon, lat)
-xyz <- cbind(lon, lat, 1)
+#xyz <- cbind(lon, lat, 1)
 xyzt <- cbind(lon, lat, 1, 0)
 test_that("input checks work", {
   if (!ok_proj6()) skip("no PROJ6 available, no real testing to do")
 
-  expect_error(proj_trans_generic(xyzt), "argument \"target\" is missing, with no default")
-  expect_error(proj_trans_generic(xyzt, dst), "source must be provided as a string")
-  expect_error(proj_trans_generic(xyzt, 1, source = llproj), "target must be a string")
-  expect_silent(proj_trans_generic(data.frame(lon, lat, 0, 0), dst, source = llproj))
-  expect_error(proj_trans_generic(cbind("a", "b", "c", "d"), dst , source  = llproj),
+  expect_error(proj_trans_generic(xy), "argument \"target\" is missing, with no default")
+  expect_error(proj_trans_generic(xy, dst), "source must be provided as a string")
+  expect_error(proj_trans_generic(xy, 1, source = llproj), "target must be a string")
+  expect_silent(proj_trans_generic(data.frame(lon, lat), dst, source = llproj))
+  expect_error(proj_trans_generic(cbind("a", "b"), dst , source  = llproj),
                "input coordinates must be numeric")
 
   expect_error(proj_trans_generic(cbind(numeric(0), numeric(0)), dst , source  = llproj),
                "must be at least one coordinate")
   expect_error(proj_trans_generic(matrix(1:3, 3:1)[NA, , drop   = FALSE], dst , source  = llproj),
-              "coordinates must be 2-columns, 3-columns or 4-columns")
+              "x coordinates must be 2-columns")
 
   expect_silent(proj_trans_generic(xy, dst, source = llproj))
-  expect_silent(proj_trans_generic(xyz, dst, source = llproj))
-  expect_silent(proj_trans_generic(xyzt, dst, source = llproj))
+  expect_silent(proj_trans_generic(xy, z_ = 0, dst, source = llproj))
+  expect_silent(proj_trans_generic(xy, z_ = 0, t_ = 1, dst, source = llproj))
 
-  expect_silent(proj_trans_generic(cbind(xyzt, 2, 1), dst, source = llproj))
+  expect_error(proj_trans_generic(cbind(xyzt, 2, 1), dst, source = llproj))
 
 })
 
