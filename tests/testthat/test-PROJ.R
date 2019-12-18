42ce145c1371f8af1c71e807327bcc534d5ab4df
Michael Sumner <mdsumner@gmail.com>
2019-12-18 05:14:32 +0000

    list-io

--- tests/testthat/test-PROJ.R
+++ tests/testthat/test-PROJ.R
@@ -8,13 +8,13 @@ dst <- "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42"
 test_that("PROJ works", {
   if (!ok_proj6()) skip("no PROJ6 available, no real testing to do")
 
-  expect_silent(xyz <- proj_trans_generic(cbind(X = lon, Y = lat, Z = rep(0, length(lon))), dst, source = "WGS84"))
-  expect_silent(lonlat <- proj_trans_generic(cbind(X = xyz[, 1L], Y = xyz[, 2L], Z = rep(0, length(lon))),
+  expect_silent(xyz <- proj_trans_generic(cbind(X = lon, Y = lat), z_ = rep(0, length(lon)), dst, source = "WGS84"))
+  expect_silent(lonlat <- proj_trans_generic(cbind(X = xyz["x_"], Y = xyz["y_"]), z_ = rep(0, length(lon)),
                                      "WGS84", source = dst))
-  expect_true(all(lonlat[, 1L] <= 180))
-  expect_true(all(lonlat[, 1L] > -180))
-  expect_true(all(lonlat[, 2L] < 90))
-  expect_true(all(lonlat[, 2L] > -90))
+  expect_true(all(lonlat[["x_"]] <= 180))
+  expect_true(all(lonlat[["x_"]] > -180))
+  expect_true(all(lonlat[["y_"]] < 90))
+  expect_true(all(lonlat[["y_"]] > -90))
 
 
 })
