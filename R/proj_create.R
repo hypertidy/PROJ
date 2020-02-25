005181837849291455867f57648a2a56e22a71d4
Michael Sumner <mdsumner@gmail.com>
2020-02-25 22:22:18 +0000

    add stub for proj_create()

---
+++ R/proj_create.R
@@ -0,0 +1,13 @@
+proj_create <- function() {
+  stop("not functional")
+  #.C("PROJ_proj_create", crs_ = "+proj=longlat", format = 2L, success = 0L)
+  # PROJJSON
+  # $crs_
+  # [1] "+proj=longlat"
+  #
+  # $format
+  # [1] 2
+  #
+  # $success
+  # [1] 1
+}
