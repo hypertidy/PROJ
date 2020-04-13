c073d666a29ef5cf000388052e46244ec9b049f7
Michael Sumner <mdsumner@gmail.com>
2020-04-13 19:59:31 +1000

    exercise windows safety

---
+++ tests/testthat/test-no-crash.R
@@ -0,0 +1,17 @@
+x <- cbind(c(100.25, 100.75, 101.25, 101.75, 102.25, 102.75),
+           100.25)
+
+
+source <-  "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+target <- "+proj=merc +datum=WGS84"
+
+
+test_that("out of bounds works", {
+  a <- expect_output({
+
+    proj_trans_generic(x, target, source = source)
+
+  })
+
+  expect_equal(unique(a$z_), 0)
+})
