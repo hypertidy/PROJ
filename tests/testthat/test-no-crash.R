83c430cfc6a062a48006919601b21f7c6dccbfa6
Michael Sumner <mdsumner@gmail.com>
2020-04-13 20:13:40 +1000

    weekend comes

--- tests/testthat/test-no-crash.R
+++ tests/testthat/test-no-crash.R
@@ -5,9 +5,9 @@ x <- cbind(c(100.25, 100.75, 101.25, 101.75, 102.25, 102.75),
 source <-  "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 target <- "+proj=merc +datum=WGS84"
 
-
+suppressWarnings(is_ok <- ok_proj6())
 test_that("out of bounds works", {
-  skip_if_not(ok_proj6())
+  skip_if_not(is_ok)
   a <- expect_output({
 
     proj_trans_generic(x, target, source = source)
