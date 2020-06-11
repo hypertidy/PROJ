f24a21331845bf0a4c58761050fb16b90b7346a1
Michael Sumner <mdsumner@gmail.com>
2020-06-11 07:38:25 +0000

    proj_trans_xy()

---
+++ R/proj_trans.R
@@ -0,0 +1,3 @@
+PROJ_proj_trans_xy <- function(x_, y_, src_, tgt_) {
+ # .Call("PROJ_proj_trans_xy", x_ = as.double(x_), y_ = as.double(y_), src_ = as.character(src_), tgt_ = as.character(tgt_), PACKAGE = "PROJ")
+}
