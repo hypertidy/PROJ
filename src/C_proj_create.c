bb8d6eb10fbc195dd593670fd40f6f2e8b46eab3
Michael Sumner <mdsumner@gmail.com>
2020-08-22 00:53:53 +0000

    proj_create

--- src/C_proj_create.c
+++ src/C_proj_create.c
@@ -1,4 +1,7 @@
 #include <libproj.h>
+
+
+#include <R.h>
 #include <Rinternals.h>
 
 
@@ -24,19 +27,17 @@ SEXP proj_create_text(SEXP crs_, SEXP format)
     error(proj_errno_string(proj_errno(0)));
   if (fmt == 0L) {
     // available types
-    // PJ_WKT1_ESRI, PJ_WKT1_GDAL, PJ_WKT2_2015, PJ_WKT2_2015_SIMPLIFIED, PJ_WKT2_2018, PJ_WKT2_2018_SIMPLIFIED;
-// FIXME: MDS cannot figure this out
-    //outstring = proj_as_wkt(0, pj, PJ_WKT2_2018, NULL);
-    //outstring = "";
+    // PJ_WKT1_ESRI, PJ_WKT1_GDAL, PJ_WKT2_2015, PJ_WKT2_2015_SIMPLIFIED, PJ_WKT                                          2_2018, PJ_WKT2_2018_SIMPLIFIED;
+    outstring = proj_as_wkt(0, pj, PJ_WKT2_2018, NULL);
+
     success = 1L;
   }
-  // disabled, seems not to work in PROJ 6.3.1 (but I can't find definitive doc)
-  // if (fmt == 1L) {
-  //PJ_PROJ_4, PJ_PROJ_5;
-  //   outstring = proj_as_proj_string(0, pj, PJ_PROJ_5, NULL);
+  if (fmt == 1L) {
+    //PJ_PROJ_4, PJ_PROJ_5;
+    outstring = proj_as_proj_string(0, pj, PJ_PROJ_5, NULL);
 
-  //   success = 1L;
-  // }
+    success = 1L;
+  }
   //if (fmt ==  2L) {
   // disabled for now 2010-02-26 (needs PROJ 6.2.0 and rwinlib is at 6.1.0)
   //outstring = proj_as_projjson(0, pj, NULL);
@@ -45,14 +46,14 @@ SEXP proj_create_text(SEXP crs_, SEXP format)
   // form output as a character vector *before* destroying proj object
   // The returned string is valid while the input obj parameter is valid, and
   // until a next call to proj_as_*() with the same input object.
-  // https://proj.org/development/reference/functions.html#_CPPv411proj_as_wktP10PJ_CONTEXTPK2PJ11PJ_WKT_TYPEPPCKc
+  // https://proj.org/development/reference/functions.html#_CPPv411proj_as_wktP1                                          0PJ_CONTEXTPK2PJ11PJ_WKT_TYPEPPCKc
 
   SET_STRING_ELT(out, 0, mkChar(outstring));
 
   proj_destroy(pj);
 
+
   UNPROTECT(1);
 
   return(out);
 }
-
