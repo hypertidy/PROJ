36e3c7c2192ca529e0a6c431d2155e48ec940154
Anthony North <anthony.jl.north@gmail.com>
2023-12-04 07:23:17 +0000

    proj 6 hard dependency

--- src/C_proj_version.c
+++ src/C_proj_version.c
@@ -1,8 +1,6 @@
 #include <R.h>
 #include <Rinternals.h>
 
-#ifdef USE_PROJ6_API
-
 #include <proj.h>
 
 #define STR_HELPER(x) #x
@@ -14,15 +12,3 @@ SEXP C_proj_version(void)
 {
   return Rf_mkString(PROJ_PROJ_VERSION);
 }
-
-#else
-
-SEXP C_proj_version(void)
-{
-  SEXP out = PROTECT(allocVector(STRSXP, 1));
-  SET_STRING_ELT(out, 0, NA_STRING);
-  UNPROTECT(1);
-  return out;
-}
-
-#endif
