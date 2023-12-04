e0c4b8157d7b2336466d09f37c220f976b8e8342
Anthony North <anthony.jl.north@gmail.com>
2023-12-04 08:19:16 +0000

    deprecate ok_proj6

--- tests/testthat/test-PROJ.R
+++ tests/testthat/test-PROJ.R
@@ -6,7 +6,6 @@ lat <- na.omit(w[,2])
 dst <- "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42"
 
 test_that("PROJ works", {
-skip_if(!ok_proj6())
   src <- "+proj=longlat +datum=WGS84"
   expect_silent(xyz <- proj_trans(cbind(X = lon, Y = lat), z_ = rep(0, length(lon)), dst, source = src))
   expect_silent(lonlat <- proj_trans(cbind(X = xyz["x_"], Y = xyz["y_"]), z_ = rep(0, length(lon)),
--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -1,15 +1,7 @@
 test_that("create works", {
-  testthat::skip_if(!ok_proj6())
     wkt2 <- proj_crs_text("+proj=longlat +datum=WGS84 +no_defs +type=crs")
 
   expect_true( grepl("^\\+proj=longlat", proj_crs_text(wkt2, format = 1)))
 
 
 })
-
-test_that("failure is graceful", {
-  ## we *do not* have PROJ lib, so we get NA
-   skip_if(ok_proj6())
-   expect_true(is.na(proj_crs_text("+proj=longlat", format = 1L)))
-   }
-)
