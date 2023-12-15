08ebb14687b173cac96b2b4877485bf499ac6d23
Anthony North <anthony.jl.north@gmail.com>
2023-12-15 23:45:53 +1000

    fix leaky proj create

--- src/wk-trans.c
+++ src/wk-trans.c
@@ -5,8 +5,7 @@
 #include "wk-v1.h"
 
 typedef struct {
-  PJ_CONTEXT* ctx;
-  PJ* proj;
+  PJ* pj;
 } proj_trans_t;
 
 static int transform(R_xlen_t feature_id, const double* xyzm_in, double* xyzm_out,
@@ -14,7 +13,7 @@ static int transform(R_xlen_t feature_id, const double* xyzm_in, double* xyzm_ou
   proj_trans_t* data = (proj_trans_t*)trans_data;
 
   PJ_COORD coord_in = proj_coord(xyzm_in[0], xyzm_in[1], xyzm_in[2], xyzm_in[3]);
-  PJ_COORD coord_out = proj_trans(data->proj, PJ_FWD, coord_in);
+  PJ_COORD coord_out = proj_trans(data->pj, PJ_FWD, coord_in);
   // FIXME: handle error
   memcpy(xyzm_out, coord_out.v, 4 * sizeof(double));
 
@@ -22,10 +21,15 @@ static int transform(R_xlen_t feature_id, const double* xyzm_in, double* xyzm_ou
 }
 
 static void finalize(void* trans_data) {
+  if (trans_data == NULL) return;
+
   proj_trans_t* data = (proj_trans_t*)trans_data;
-  proj_destroy(data->proj);
-  proj_context_destroy(data->ctx);
+  proj_destroy(data->pj);
   free(data);
+}
+
+static void ctx_xptr_destroy(SEXP ctx_xptr) {
+  proj_context_destroy((PJ_CONTEXT*)R_ExternalPtrAddr(ctx_xptr));
 }
 
 #if PROJ_VERSION_MAJOR < 8
@@ -50,27 +54,35 @@ SEXP C_proj_trans_new(SEXP source_crs, SEXP target_crs, SEXP use_z, SEXP use_m) 
   trans->use_z = LOGICAL_RO(use_z)[0];
   trans->use_m = LOGICAL_RO(use_m)[0];
 
-  proj_trans_t* data = (proj_trans_t*)malloc(sizeof(proj_trans_t));
+  proj_trans_t* data = (proj_trans_t*)calloc(1, sizeof(proj_trans_t));
   if (data == NULL) {
     free(trans);
     Rf_error("Can't allocate proj_trans_t");
   }
 
-  data->ctx = proj_context_create();
-  PJ* pj = proj_create_crs_to_crs(data->ctx, CHAR(STRING_ELT(source_crs, 0)),
+  trans->trans_data = data;
+
+  PJ_CONTEXT* ctx = proj_context_create();
+  // ensure context is finalised
+  SEXP ctx_xptr = PROTECT(R_MakeExternalPtr(ctx, R_NilValue, R_NilValue));
+  R_RegisterCFinalizer(ctx_xptr, &ctx_xptr_destroy);
+
+  PJ* pj = proj_create_crs_to_crs(ctx, CHAR(STRING_ELT(source_crs, 0)),
                                   CHAR(STRING_ELT(target_crs, 0)), NULL);
   if (pj == NULL) {
-    stop_proj_error(data->ctx);
+    wk_trans_destroy(trans);
+    stop_proj_error(ctx);
   }
 
   // always lon,lat
-  data->proj = proj_normalize_for_visualization(data->ctx, pj);
-  if (data->proj == NULL) {
-    stop_proj_error(data->ctx);
+  data->pj = proj_normalize_for_visualization(ctx, pj);
+  proj_destroy(pj);
+  if (data->pj == NULL) {
+    wk_trans_destroy(trans);
+    stop_proj_error(ctx);
   }
 
-  proj_destroy(pj);
-  trans->trans_data = data;
-
-  return wk_trans_create_xptr(trans, R_NilValue, R_NilValue);
+  UNPROTECT(1);
+  // ensure context stays valid
+  return wk_trans_create_xptr(trans, ctx_xptr, R_NilValue);
 }
