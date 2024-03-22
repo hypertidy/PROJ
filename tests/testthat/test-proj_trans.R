57122f504dddcc97eda37eef4108a61fdb7abd7c
Anthony North <anthony.jl.north@gmail.com>
2024-03-22 07:14:41 +0000

    migrate out-of-bounds tests

--- tests/testthat/test-proj_trans.R
+++ tests/testthat/test-proj_trans.R
@@ -1,7 +1,7 @@
 test_that("proj_trans() works", {
   xymap_trans <- proj_trans(
-    xymap, 
-    "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42 +type=crs", 
+    xymap,
+    "+proj=laea +datum=WGS84 +lon_0=147 +lat_0=-42 +type=crs",
     "+proj=longlat +datum=WGS84 +type=crs"
   )
 
@@ -296,7 +296,7 @@ test_that("proj_trans.wk_handleable() works", {
       data.frame(
         foo = "bar",
         point = wk::xyzm(-1:1, -1:1, -1:1, -1:1, "OGC:CRS84")
-      ), 
+      ),
       "EPSG:3857"
     ),
     data.frame(
@@ -330,7 +330,7 @@ test_that("proj_trans.sf() works", {
         y = c(-111325.1429, 0, 111325.1429)
       ),
       coords = c("x", "y"),
-      crs = "EPSG:3857"  
+      crs = "EPSG:3857"
     )
   )
 })
@@ -351,6 +351,43 @@ test_that("proj_trans.sfc() works", {
       sf::st_point(c(0, 0)),
       sf::st_point(c(111319.4908, 111325.1429)),
       crs = "EPSG:3857"
+    )
+  )
+})
+
+test_that("proj_trans() handles out-of-bounds coordinates", {
+  trans_a <- expect_no_error(
+    proj_trans(
+      cbind(c(100.25, 100.75, 101.25, 101.75, 102.25, 102.75), 100.25),
+      "+proj=merc +datum=WGS84 +type=crs",
+      "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0 +type=crs"
+    )
+  )
+
+  expect_true(!all(is.finite(trans_a[, 1])))
+
+  trans_b <- expect_no_error(
+    proj_trans(cbind(2e10, 2e12),
+      source = "+proj=stere +datum=WGS84 +type=crs",
+      target = "+proj=laea +datum=WGS84 +type=crs"
+    )
+  )
+
+  expect_true(is.matrix(trans_b))
+  expect_true(all(!is.finite(trans_b)))
+
+
+  expect_silent(
+    proj_trans(cbind(NA_real_, NA_real_),
+      target = "+proj=longlat +datum=WGS84 +type=crs",
+      source = "+proj=laea +datum=WGS84 +type=crs"
+    )
+  )
+
+  expect_silent(
+    proj_trans(cbind(c(1, NA_real_, NaN), c(1e6, NA_real_, NaN)),
+      target = "+proj=longlat +datum=WGS84 +type=crs",
+      source = "+proj=laea +datum=WGS84 +type=crs"
     )
   )
 })
